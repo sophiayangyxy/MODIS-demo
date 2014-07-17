b50545089dd02814d61daeb38edf00ee8367c4da
Yangxinye Yang <yangyangxinye@gmail.com>
2014-07-17 10:56:52 -0500

    update modis01

--- original/modis/tutorial/modis01.swift
+++ original/modis/tutorial/modis01.swift
@@ -1,13 +1,17 @@
 type imagefile;
 type landuse;
+type get_script;
 
-app (landuse output) getLandUse (imagefile input)
+app (landuse output) getLandUse (imagefile input, get_script script)
+
 {
-  getlanduse filename(input) stdout=filename(output);
+  bash filename(script) filename(input) stdout=filename(output);
 }
 
 // imagefile modisImage <"data/europe/h18v05.rgb">;
-imagefile modisImage<"../data/modis/2002/h00v08.rgb">;
+imagefile modisImage<"../data/h00v08.rgb">;
 // landuse result <"landuse/h18v05.landuse.byfreq">;
 landuse result <"landuse/h00v08.landuse.byfreq">;
-result = getLandUse(modisImage);
+get_script script<"../bin/getlanduse.sh">;
+
+result = getLandUse(modisImage, script);
