ed43acbd09e5e293b3db1246b19113e923a89cce
Yangxinye Yang <yangyangxinye@gmail.com>
2014-07-21 15:33:24 +0000

    update to files

--- original/modis/bin/getlanduse.sh
+++ original/modis/bin/getlanduse.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$BINDIR/rgb_histogram.pl $1 | sort -rn
+#BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#$BINDIR/rgb_histogram.pl $1 | sort -rn
+chmod +x rgb_histogram.pl
+./rgb_histogram.pl $1 | sort -rn
--- original/modis/tutorial/modis01.swift
+++ original/modis/tutorial/modis01.swift
@@ -1,17 +1,18 @@
 type imagefile;
 type landuse;
 type get_script;
+type file;
 
-app (landuse output) getLandUse (imagefile input, get_script script)
-
+app (landuse output) getLandUse (imagefile input, get_script script, file rgb_script)
 {
   bash filename(script) filename(input) stdout=filename(output);
 }
 
 // imagefile modisImage <"data/europe/h18v05.rgb">;
 imagefile modisImage<"../data/h00v08.rgb">;
-// landuse result <"landuse/h18v05.landuse.byfreq">;
-landuse result <"landuse/h00v08.landuse.byfreq">;
+// landuse result <"landuse1/h18v05.landuse.byfreq">;
+landuse result <"landuse1/h00v08.landuse.byfreq">;
 get_script script<"../bin/getlanduse.sh">;
+file rgb_script<"./rgb_histogram.pl">;
 
-result = getLandUse(modisImage, script);
+result = getLandUse(modisImage, script, rgb_script);
