7b2a8c55db77f74e5c2639347e5bcfd5e54ab140
Yangxinye Yang <sophiayang@midway-login1.rcc.local>
2014-07-28 10:12:42 -0500

    add europe setup

--- original/modis/tutorial/setup.sh
+++ original/modis/tutorial/setup.sh
@@ -2,9 +2,16 @@
 
 cat /etc/skel/.bashrc >> ~/.bashrc
 
+#source ~/.bashrc
+#mkdir ../data
+#cd ../data
+#wget --no-check-certificate http://www.ci.uchicago.edu/swift/modis/modis-2002.tar.gz
+#tar -zxf modis-2002.tar.gz
+#cd ../tutorial
+
 source ~/.bashrc
-mkdir ../data
-cd ../data
-wget --no-check-certificate http://www.ci.uchicago.edu/swift/modis/modis-2002.tar.gz
-tar -zxf modis-2002.tar.gz
-cd ../tutorial
+mkdir -p ../data/modis/2002
+cd ../data/modis/2002
+wget --no-check-certificate http://www.ci.uchicago.edu/swift/modis/europe.tar.gz
+tar -zxf europe.tar.gz
+cd -
